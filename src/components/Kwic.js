096f39c49e1dea4a2fbac9aa82a7a7e706b51d46
neongala5 <carlosmartz555@gmail.com>
2022-10-12 21:40:14 -0500

    added cards

--- src/components/Kwic.js
+++ src/components/Kwic.js
@@ -6,7 +6,9 @@ class Kwic extends React.Component {
         super(props);
         this.state = {
             userInput: "",
-            circularShifts: []
+            circularShifts: [],
+            alphabetizedList: [],
+            userInputArrayList: []
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -20,33 +22,39 @@ class Kwic extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
         if (this.state.userInput.trim() === '') {
-            console.log("Only white space or nothing was entered")
+            alert("Only white space or nothing was entered")
             this.setState({
                 userInput: "",
-                circularShifts: []
+                alphabetizedList: [],
+                circularShifts: [],
+                userInputArrayList: []
             });
         } else {
-            console.log("User input is: \n" + this.state.userInput + "\n\n");
-            console.log("Circular shifts: \n")
 
             let userInputArray = this.state.userInput.split("\n");
             let allCircularShifts = [];
+            let allAlphabetized = [];
+
+            this.setState({
+                userInputArrayList: userInputArray
+            });
 
             userInputArray.forEach(element => {
                 let wordsInThisLine = element.trim().split(" ");
                 wordsInThisLine.forEach(line => {
-                    console.log(wordsInThisLine.join(' '))
-                    this.arrayRotate(wordsInThisLine);
                     allCircularShifts.push(wordsInThisLine.join(' '));
+                    this.arrayRotate(wordsInThisLine);
                 });
             });
-            allCircularShifts.sort(function (a, b) {
-                return a.toLowerCase().localeCompare(b.toLowerCase());
-            });
-            console.log("\nAlphebetized list:")
-            console.log(allCircularShifts.join('\n'));
             this.setState({
                 circularShifts: allCircularShifts
+            });
+            allAlphabetized = [...allCircularShifts];
+            allAlphabetized.sort(function (a, b) {
+                return a.toLowerCase().localeCompare(b.toLowerCase());
+            });
+            this.setState({
+                alphabetizedList: allAlphabetized
             });
         }
     }
@@ -60,13 +68,34 @@ class Kwic extends React.Component {
         return (
             <form onSubmit={this.handleSubmit} className="App">
                 <textarea type="text" rows="3" placeholder="Enter lines of text you wish to circularly shift" name="body" value={this.state.userInput} onChange={this.onChange} />
-                <input type="submit"/>
-                {this.state.circularShifts.map((answer, i) => {
-                    // Return the element. Also pass key     
-                    return (<p key={answer+i} >{answer}</p>)
-                })}
+                <input type="submit" />
+                <div className='cardContainer'>
+                    <div className="card">
+                        <div className="container">
+                            <h4><b>User Input Is:</b></h4>
+                            {this.state.userInputArrayList.map((answer, i) => {
+                                return (<p key={answer + i} >{answer}</p>)
+                            })}
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="container">
+                            <h4><b>Circular shifts:</b></h4>
+                            {this.state.circularShifts.map((answer, i) => {
+                                return (<p key={answer + i} >{answer}</p>)
+                            })}
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="container">
+                            <h4><b>Alphabetized list:</b></h4>
+                            {this.state.alphabetizedList.map((answer, i) => {
+                                return (<p key={answer + i} >{answer}</p>)
+                            })}
+                        </div>
+                    </div>
+                </div>
             </form>
-
         );
     }
 }
